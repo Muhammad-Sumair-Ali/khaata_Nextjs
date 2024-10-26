7541fedbb43362463a735374deea3c7589a993b5
Muhammad-Sumair-Ali <sumairjattoi@gmail.com>
2024-10-27 01:33:08 +0500

    v2-update

--- src/app/singlepage/[id]/page.tsx
+++ src/app/singlepage/[id]/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from "react";
+import React from "react";
 import  { useFetch } from "@/app/action/customer";
 import { useParams } from "next/navigation"; 
 import AddTransaction from "@/app/components/AddTransaction";
