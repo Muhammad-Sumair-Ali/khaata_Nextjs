4cacfa5dcbc70707aeac940d9f2311e1a9dd29df
Muhammad-Sumair-Ali <sumairjattoi@gmail.com>
2024-10-27 03:38:42 +0500

    first

--- src/app/api/customers/addtransections/[id]/route.ts
+++ src/app/api/customers/addtransections/[id]/route.ts
@@ -1,18 +1,23 @@
 import { connectDb } from "@/dbConfig/dbConfig";
 import Customer from "@/models/customerModel";
 import { NextRequest, NextResponse } from "next/server";
-export async function POST(request: NextRequest, { params }: { params: { id: string } }) {
-   connectDb();
 
-  const customerId = params.id; 
-  const { amount, type, details } = await request.json(); 
+export async function POST(request: NextRequest, { params }: { params: { id: string } }) {
+  connectDb();
+
+  const customerId = params.id;
 
   try {
+
+    const { amount, type, details } = await request.json();
+    if (!amount || !type || !details) {
+      return NextResponse.json({ message: 'Missing required fields' }, { status: 400 });
+    }
+
     const customer = await Customer.findById(customerId);
     if (!customer) {
       return NextResponse.json({ message: 'Customer not found' }, { status: 404 });
     }
-
 
     const newTransaction = { amount, type, details };
     customer.transactions.push(newTransaction);
@@ -26,6 +31,7 @@ export async function POST(request: NextRequest, { params }: { params: { id: str
     await customer.save();
     return NextResponse.json({ message: 'Transaction added successfully!', customer }, { status: 200 });
   } catch (error) {
+    console.error("Error adding transaction:", error); // Log error for debugging
     return NextResponse.json({ message: error.message }, { status: 500 });
   }
 }
