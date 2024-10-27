4c6885326f4baa850a55c05ac8f70fa751b763a7
Muhammad-Sumair-Ali <sumairjattoi@gmail.com>
2024-10-28 00:52:33 +0500

    new_1

--- src/app/context/AuthContext.tsx
+++ src/app/context/AuthContext.tsx
@@ -1,30 +1,52 @@
-'use client'
+'use client';
 import React, { createContext, useContext, useEffect, useState } from 'react';
 
-const AuthContext = createContext();
 
-export const AuthProvider = ({ children }:any ) => {
-  const [user, setUser] = useState({
-    user: null,
-    token: "",
-  });
+interface User {
+    email: string;
+    username: string;
+  
+}
 
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    const userData = localStorage.getItem('user');
-    
-    if (token && userData) {
-      setUser({
-        user: JSON.parse(userData),
-        token: token,
-      });
-    }
-  }, []);
-  return (
-    <AuthContext.Provider value={{ user, setUser }}>
-      {children}
-    </AuthContext.Provider>
-  );
+interface AuthContextType {
+    user: {
+        user: User | null;
+        token: string;
+    };
+    setUser: React.Dispatch<React.SetStateAction<{ user: User | null; token: string; }>>;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [user, setUser] = useState<{ user: User | null; token: string }>({
+        user: null,
+        token: "",
+    });
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        const userData = localStorage.getItem('user');
+
+        if (token && userData) {
+            setUser({
+                user: JSON.parse(userData),
+                token: token,
+            });
+        }
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user, setUser }}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (context === undefined) {
+        throw new Error('useAuth must be used within an AuthProvider');
+    }
+    return context;
+};
